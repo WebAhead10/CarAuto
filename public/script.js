503bfc98d39e668a0cfe78fc4ac459236a543719
EliasSalom <elias.29@hotmail.com>
2021-07-01 13:02:57 +0300

    create elemnt

--- public/script.js
+++ public/script.js
@@ -5,10 +5,9 @@
 // loop on words that match
 // make a connection between back-end and the front-end
 
-// const div = document.createElement("div");
+const div1 = document.getElementById("proprty");
 const input = document.getElementById("searchbar");
 const op = document.getElementById("brow");
-// const op = document.getElementsByTagName("datalist");
 
 
 input.addEventListener("input", (event) => {
@@ -30,10 +29,33 @@ input.addEventListener("input", (event) => {
         option.className="optionbar";
         console.log(option.value);
         op.appendChild(option);
-        
+        if(input.value==element.Name)
+        {
+          create(element);
+        }
       });
     })
     .catch((err) => {
       console.log(err);
     });
 });
+
+function create(a)
+{
+  var element = [5];
+  for(let i=0; i < 5 ;i++)
+  {
+    element[i]=document.createElement("label");
+    // element[i].id=`ele${i}`;
+  }
+  element[0].textcontent=a.Name;
+  element[1].textcontent=a.Miles_per_Gallon;
+  element[2].textcontent=a.Horsepower;
+  element[3].textcontent=a.Year;
+  element[4].textcontent=a.Origin;
+  
+  for(let i=0; i < 5 ;i++)
+  {
+    div1.appendChild(element[i]);
+  }
+}
